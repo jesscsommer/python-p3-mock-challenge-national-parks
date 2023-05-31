269a86d63a6b5176dd63a807dbab16f919992b26
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-05-30 19:48:20 -0700

    Complete mock CC

--- lib/classes/national_park.py
+++ lib/classes/national_park.py
@@ -3,18 +3,41 @@ class NationalPark:
     def __init__(self, name):
         self.name = name
         self._trips = []
-        self._visitors = []
+        self._visitors = set()
         
     def trips(self, new_trip=None):
         from classes.trip import Trip
-        pass
+        if isinstance(new_trip, Trip):
+            self._trips.append(new_trip)
+        return self._trips
     
     def visitors(self, new_visitor=None):
         from classes.visitor import Visitor
-        pass
+        if isinstance(new_visitor, Visitor):
+            self._visitors.add(new_visitor)
+        return list(self._visitors)
     
     def total_visits(self):
-        pass
+        return len(self._trips)
     
     def best_visitor(self):
-        pass
+        most_visits = 0
+        best_visitor = None
+        for visitor in self._visitors:
+            count = len([trip for trip in self._trips if trip.visitor == visitor])
+            if count and count > most_visits:
+                most_visits = count
+                best_visitor = visitor
+        return best_visitor
+
+    # Properties 
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if not hasattr(self, "name") and type(name) == str:
+            self._name = name
+        else: 
+            raise Exception 
--- lib/classes/visitor.py
+++ lib/classes/visitor.py
@@ -2,11 +2,30 @@ class Visitor:
 
     def __init__(self, name):
         self.name = name
+        self._trips = []
+        self._national_parks = set()
         
     def trips(self, new_trip=None):
         from classes.trip import Trip
-        pass
+        if isinstance(new_trip, Trip):
+            self._trips.append(new_trip) 
+        return self._trips
     
     def national_parks(self, new_national_park=None):
         from classes.national_park import NationalPark
-        pass
+        if isinstance(new_national_park, NationalPark):
+            self._national_parks.add(new_national_park)
+        return list(self._national_parks)
+
+    # Properties
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if not hasattr(self, "name") and (type(name) == str) and (1 <= len(name) <= 15):
+            self._name = name
+        else: 
+            raise Exception 
+    
